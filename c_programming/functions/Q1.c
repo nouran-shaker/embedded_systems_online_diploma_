82ea0f6f0f64ad2b7486bfab4dd071cdc59e87e3
nouran-shaker <nouranshaker50@gmail.com>
2025-08-06 16:22:43 +0300

    Create Q1.c

---
+++ c_programming/functions/Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+void prime_numbers(int a, int b) {
+   
+	for (int i = a; i < b; i++) {
+	     int temp=0;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+			   temp++;
+			}
+		}
+		if (temp==0){
+		    printf("%d\t",i);
+		}
+	}}
+
+
+int main() {
+	int x, y;
+	printf("Enter two numbers (intervals):");
+	fflush(stdout); fflush(stdin);
+	scanf(" %d %d", &x, &y);
+	printf("Prime numbers between %d and %d are: ");
+	prime_numbers(x, y);
+
+}
